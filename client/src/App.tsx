c9c5e5ecefe4febf4fe7e37453ff3f25f98976cd
efthimispegas <efthimispegas@gmail.com>
2021-10-05 18:56:31 +0300

    [client]: Implement layout ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import Layout from './components/Layout/Layout';
+import theme from './theme';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <Layout />
+        </CssBaseline>
+      </ThemeProvider>
+    </React.StrictMode>
   );
 }
 
