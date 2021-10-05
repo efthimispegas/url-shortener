263d11b70567caf8ba76f28ed4db5f21ff8852d8
efthimispegas <efthimispegas@gmail.com>
2021-10-05 19:06:26 +0300

    [client]: Add header & footer component

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,46 @@
+import { AppBar, Grid, makeStyles, createStyles, Toolbar, Typography } from '@material-ui/core';
+import React from 'react';
+import logo from '../../assets/logo.svg';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    header: {
+      background: theme.palette.primary.main,
+    },
+    logo: {
+      width: 32,
+      height: 32,
+      margin: theme.spacing(0, 1),
+    },
+    toolbar: theme.mixins.toolbar,
+    text: {
+      marginRight: 10,
+      marginLeft: 10,
+    },
+    link: {},
+  }),
+);
+
+const Header: React.FC = () => {
+  const classes = useStyles();
+  return (
+    <AppBar position="fixed" variant="elevation" className={classes.header}>
+      <Toolbar className={classes.toolbar}>
+        <Grid container alignItems="center">
+          <Grid item>
+            <img src={logo} alt="url-shortener-logo" className={classes.logo} />
+          </Grid>
+          <Grid item container xs={11} justify="flex-start">
+            <Grid item>
+              <Typography variant="h1" noWrap>
+                Free URL Shortener
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
-import { AppBar, Grid, makeStyles, createStyles, Toolbar, Typography, Button } from '@material-ui/core';
 import React from 'react';
-import logo from '../../assets/logo.svg';
+import { makeStyles, createStyles } from '@material-ui/core';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -12,21 +13,9 @@ const useStyles = makeStyles((theme) =>
       height: 32,
       margin: theme.spacing(0, 1),
     },
-    title: {},
-    content: {},
     toolbar: theme.mixins.toolbar,
+    content: {},
     children: {},
-    footer: {
-      top: 'auto',
-      bottom: 0,
-      height: 48,
-      justifyContent: 'center',
-    },
-    text: {
-      marginRight: 10,
-      marginLeft: 10,
-    },
-    link: {},
   }),
 );
 
@@ -34,58 +23,12 @@ const Layout: React.FC = ({ children }) => {
   const classes = useStyles();
   return (
     <div>
-      {/* Header */}
-      <AppBar position="fixed" variant="elevation" className={classes.appBar}>
-        <Toolbar className={classes.toolbar}>
-          <Grid container alignItems="center">
-            <Grid item>
-              <img src={logo} alt="url-shortener-logo" className={classes.logo} />
-            </Grid>
-            <Grid item container xs={11} justify="flex-start">
-              <Grid item>
-                <Typography className={classes.title} variant="h1" noWrap>
-                  Free URL Shortener
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-      {/* // Children */}
+      <Header />
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <div className={classes.children}>{children}</div>
       </main>
-      {/* Footer */}
-      <AppBar position="fixed" color="secondary" variant="outlined" className={classes.footer}>
-        <Grid item container alignItems="center" justify="space-between">
-          <Grid item container xs={3} justify="center">
-            <Grid item>
-              <Typography className={classes.text} variant="h4" noWrap>
-                All rights reserved © 2021
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item container xs={6} justify="center">
-            <Grid item>
-              <Typography className={classes.text} variant="h4" noWrap>
-                Made with{' '}
-                <span role="img" aria-label="developer">
-                  ❤️
-                </span>{' '}
-                Tim Pegas
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid item container xs={3} justify="center">
-            <Grid item>
-              <Button variant="text" href="https://github.com/efthimispegas" disableRipple>
-                <Typography className={classes.link} variant="h4" noWrap>Github</Typography>
-              </Button>
-            </Grid>
-          </Grid>
-        </Grid>
-      </AppBar>
+      <Footer />
     </div>
   );
 };
