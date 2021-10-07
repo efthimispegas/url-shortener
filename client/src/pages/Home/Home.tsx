a8c7c98c8f83ea430867cb11ca6efece4f89b13b
efthimispegas <efthimispegas@gmail.com>
2021-10-07 20:24:31 +0300

    [client]: Implement copy to clipboard logic

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Container, createStyles, Grid, makeStyles, TextField, Theme, Typography } from '@material-ui/core';
-import { useHistory } from 'react-router';
+import { Box, Grid, Theme, Container, makeStyles, Typography, createStyles } from '@material-ui/core';
 import { shortenUrl } from '../../api/requests/url';
 import { initialize } from '../../utils/animation';
+import CopyToClipBoard from '../../components/CopyToClipBoard/CopyToClipboard';
 import Animation from '../../components/Animation/Animation';
-import { description } from '../../data/content/text';
 import Form from '../../components/Form/Form';
-import _theme from '../../theme';
-import { redirect } from '../../api/requests/redirect';
+import { description } from '../../data/content/text';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -19,7 +17,6 @@ const useStyles = makeStyles((theme: Theme) =>
       width: '80%',
       flexDirection: 'column',
     },
-    shortUrl: {},
     description: {
       textAlign: 'center',
       margin: theme.spacing(4, 8, 0, 8),
@@ -45,11 +42,10 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const Home: React.FC = () => {
   const classes = useStyles();
-  const history = useHistory();
 
   // Local state
   const [url, setUrl] = useState<string>('');
-  const [code, setCode] = useState<string>('');
+  const [open, setOpen] = useState<boolean>(false);
   const [shortUrl, setShortUrl] = useState<string>('');
   const [error, setError] = useState<{ error: boolean; message: string | null }>({ error: false, message: null });
 
@@ -60,11 +56,9 @@ const Home: React.FC = () => {
   const handleSubmit = async () => {
     // Reset state
     setError({ error: false, message: null });
-    setCode('');
     setShortUrl('');
     try {
       const { data } = await shortenUrl({ longUrl: url });
-      setCode(data.urlCode);
       setShortUrl(`${process.env.REACT_APP_API_URL}/redirect/${data.urlCode}`);
     } catch (e: any) {
       setError({ error: true, message: 'Invalid entry' });
@@ -77,23 +71,15 @@ const Home: React.FC = () => {
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(shortUrl).then(
-      () => {
-        console.log('Async: Copying to clipboard was successful!');
-      },
+      () => setOpen(true),
       (_) => {
+        setOpen(false);
         setError({ error: true, message: 'Could not copy url to clipboard' });
       },
     );
-  };
-
-  const handleRedirect = async () => {
-    try {
-      const { data: origin } = await redirect(code);
-      // console.log(origin);
-      // window.open(origin, '_blank');
-    } catch (e: any) {
-      setError({ error: true, message: 'Something went wrong. Please try again later' });
-    }
+    setTimeout(() => {
+      setOpen(false);
+    }, 2000);
   };
 
   return (
@@ -101,25 +87,7 @@ const Home: React.FC = () => {
       <Animation />
       <Container maxWidth="md" className={classes.root}>
         <Form url={url} onClick={handleSubmit} onChange={handleChange} error={error} />
-        {shortUrl.length ? (
-          <Grid container item xs={12} justifyContent="center" className={classes.shortUrl}>
-            <Box component="form" marginY={4} minWidth={500} className={classes.box}>
-              <Grid container item>
-                <TextField
-                  fullWidth
-                  value={shortUrl}
-                  variant="outlined"
-                  onClick={handleRedirect}
-                  className={classes.shortUrl}
-                  InputProps={{
-                    readOnly: true,
-                    style: { fontWeight: 500, color: _theme.palette.secondary.main },
-                  }}
-                />
-              </Grid>
-            </Box>
-          </Grid>
-        ) : null}
+        <CopyToClipBoard open={open} value={shortUrl} onClick={copyToClipboard} />
         <Box marginY={8}>
           <Grid container item xs={12} justifyContent="center" alignItems="center">
             <Grid container item justifyContent="center" className={classes.description}>
