8836523e5cda9874f45c3863ec35bf5abd94b375
efthimispegas <efthimispegas@gmail.com>
2021-10-09 01:30:48 +0300

    [tests]: Add e2e tests

--- client/cypress/integration/Form.spec.tsx
+++ client/cypress/integration/Form.spec.tsx
@@ -1,22 +1,18 @@
 /* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="../support/index.d.ts" />
 
+import urls from '../fixtures/urls.json';
+
 context('Form', () => {
-  const URL =
-    'https://www.amazon.co.uk/dp/B07ZZW7QCM/ref=gw_uk_desk_mso_smp_shl_avs_c?pf_rd_r=J67HASZ2T6W3MDDD3JY8&pf_rd_p=98e41ac4-8217-46d8-b59c-110cb975a667&pd_rd_r=e48bffd3-4469-441b-b8b5-64495b0eaa6e&pd_rd_w=bIDlZ&pd_rd_wg=RdudP&ref_=pd_gw_unk';
-
-  const invalidURL = 'some.dummy.domain';
-
   beforeEach(() => {
     cy.visit('/');
     cy.wait(4000);
   });
 
   it('the input value matches the copied URL', () => {
-    cy.get('[data-cy="form"]')
-      .within(() => {
-        cy.get('input').should('have.class', 'MuiInputBase-input').type(URL).should('have.value', URL);
-      });
+    cy.get('[data-cy="form"]').within(() => {
+      cy.get('input').should('have.class', 'MuiInputBase-input').type(urls.validURL).should('have.value', URL);
+    });
   });
 
   it('the button is clicked', () => {
@@ -24,50 +20,35 @@ context('Form', () => {
   });
 
   it('tries to shorten an invalid URL', () => {
-    cy.shorten(invalidURL);
+    cy.shorten(urls.invalidURL);
 
     cy.get('div').contains('Invalid entry').should('be.visible');
   });
 
   it('tries to shorten a valid URL', () => {
-    cy.shorten(URL);
+    cy.shorten(urls.validURL);
 
-    cy.get('[data-cy="converted-url"]')
-      .within(() => {
-        cy
-          .get('input')
-          .should('have.attr', 'value')
-          .its('length')
-          .should('be.gt', 0);
-      });
+    cy.get('[data-cy="converted-url"]').within(() => {
+      cy.get('input').should('have.attr', 'value').its('length').should('be.gt', 0);
+    });
   });
 
   it.only('tries to copy the converted URL', () => {
     let convertedURL = '';
-    cy.shorten(URL);
-    cy
-      .get('[data-cy="copy-clipboard-btn"]')
-      .click();
-    cy
-      .get('[data-cy="converted-url"]')
-      .within(() => {
-        cy
-          .get('input')
-          .invoke('val')
-          .then((val) => {
-            convertedURL = val as string;
-            // cy.log(convertedURL as string);
-            cy.window().then((win) => {
-              win.navigator.clipboard.readText().then((text) => {
-                assert.equal(
-                  text,
-                  convertedURL,
-                );
-              });
+    cy.shorten(urls.validURL);
+    cy.get('[data-cy="copy-clipboard-btn"]').click();
+    cy.get('[data-cy="converted-url"]').within(() => {
+      cy.get('input')
+        .invoke('val')
+        .then((val) => {
+          convertedURL = val as string;
+          // cy.log(convertedURL as string);
+          cy.window().then((win) => {
+            win.navigator.clipboard.readText().then((text) => {
+              assert.equal(text, convertedURL);
             });
           });
-      });
+        });
+    });
   });
 });
-
-export {};
